f994d3d7146fbcaae885a3a5fd9ff8489fd37bac
Yaroslav Klimas <yarklim@gmail.com>
2023-11-23 19:54:23 +0200

    update modal

--- js/modal.js
+++ js/modal.js
@@ -1,24 +1,18 @@
-// (() => {
-//   const refs = {
-//     openModalBtn: document.querySelector('[data-modal-open]'),
-//     closeModalBtn: document.querySelector('[data-modal-close]'),
-//     modal: document.querySelector('[data-modal]'),
-//   };
-
-//   refs.openModalBtn.addEventListener('click', toggleModal);
-//   refs.closeModalBtn.addEventListener('click', toggleModal);
-// })();
+import { subscribeModal, productModal } from '../src/data/modal-content';
 
 const modal = document.querySelector('[data-modal]');
 const openModal = document.querySelectorAll('[data-modal-open]');
 const closeModal = document.querySelectorAll('[data-modal-close]');
+const modalContent = document.querySelector('.modal__content');
 
 openModal.forEach(el => {
   el.addEventListener('click', toggleModal);
+  document.body.classList.add('no-scroll');
 });
 
 closeModal.forEach(el => {
   el.addEventListener('click', toggleModal);
+  document.body.classList.remove('no-scroll');
 });
 
 function toggleModal() {
