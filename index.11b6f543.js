983fa9e431ae8d496b6ee4f7b699e022abb12902
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-11-23 14:37:55 +0000

    Deploying to gh-pages from @ Yarklim/organic-food@473c1caf9ef77b350ce5e98396a711da8baece35 🚀

---
+++ index.11b6f543.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),c=document.querySelectorAll("[data-modal-close]");function o(){e.classList.toggle("is-hidden")}t.forEach((e=>{e.addEventListener("click",o)})),c.forEach((e=>{e.addEventListener("click",o)}));
+//# sourceMappingURL=index.11b6f543.js.map
