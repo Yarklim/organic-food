48882e8eaf3db9db10331019e72710c2d6e5d5b1
Yarklim <105521751+Yarklim@users.noreply.github.com>
2023-11-23 17:54:49 +0000

    Deploying to gh-pages from @ Yarklim/organic-food@f994d3d7146fbcaae885a3a5fd9ff8489fd37bac 🚀

--- index.0f92fc91.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("[data-modal]"),t=document.querySelectorAll("[data-modal-open]"),c=document.querySelectorAll("[data-modal-close]");function o(){e.classList.toggle("is-hidden")}t.forEach((function(e){e.addEventListener("click",o)})),c.forEach((function(e){e.addEventListener("click",o)}))}();
-//# sourceMappingURL=index.0f92fc91.js.map
--- index.a336a21f.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".burger-menu"),n=document.querySelector(".mob-menu__nav");e.addEventListener("click",(()=>{e.classList.toggle("is-open"),n.classList.toggle("menu-open")})),document.querySelectorAll(".mob-menu__nav-link").forEach((o=>o.addEventListener("click",(o=>{e.classList.remove("is-open"),n.classList.remove("menu-open")}))));
-//# sourceMappingURL=index.a336a21f.js.map
