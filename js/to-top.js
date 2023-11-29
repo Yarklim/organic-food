8595caa610b16c84255db1b33b886b43ed1d1409
Yaroslav Klimas <yarklim@gmail.com>
2023-11-29 12:50:49 +0200

    update modal

--- js/to-top.js
+++ js/to-top.js
@@ -1,9 +1,14 @@
 const toTop = document.querySelector('.to-top__button');
 
 window.addEventListener('scroll', () => {
-  if (Math.round(window.scrollY) > 200) {
+  if (Math.round(window.scrollY) > 300) {
     toTop.classList.add('active');
   } else {
     toTop.classList.remove('active');
   }
+});
+
+toTop.addEventListener('click', e => {
+  e.preventDefault();
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
