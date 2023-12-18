238bc5c0dafe8e684f6eb640936567d9e390dac8
Yaroslav Klimas <yarklim@gmail.com>
2023-12-18 11:36:40 +0200

    update mobile-menu.js

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -4,11 +4,20 @@ const mobMenu = document.querySelector('.mob-menu__nav');
 burger.addEventListener('click', () => {
   burger.classList.toggle('is-open');
   mobMenu.classList.toggle('menu-open');
+  document.body.classList.toggle('no-scroll');
 });
 
-document.querySelectorAll('.mob-menu__nav-link').forEach(el =>
+document.querySelectorAll('.mobile-link').forEach(el =>
   el.addEventListener('click', e => {
     burger.classList.remove('is-open');
     mobMenu.classList.remove('menu-open');
+    document.body.classList.toggle('no-scroll');
   })
 );
+
+window.matchMedia('(min-width: 992px)').addEventListener('change', e => {
+  if (!e.matches) return;
+  burger.classList.remove('is-open');
+  mobMenu.classList.remove('menu-open');
+  document.body.classList.remove('no-scroll');
+});
