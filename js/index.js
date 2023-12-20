6e72b6bb7a8c615f9195384260ca797443732a69
Yaroslav Klimas <yarklim@gmail.com>
2023-12-20 14:37:00 +0200

    update modal.js

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import './mobile-menu';
 import './carousel';
+import './modal-content';
 import './modal';
 import './to-top';
--- js/modal.js
+++ js/modal.js
@@ -1,11 +1,12 @@
-import { contactModal, productModal } from './modal-content';
+// import { contactModal, productModal } from './modal-content';
 
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('[data-modal]');
 const openModalBtn = document.querySelectorAll('[data-modal-open]');
 const closeModalBtn = document.querySelectorAll('[data-modal-close]');
-const contentContainer = document.querySelector('.modal__content');
-const submitBtn = document.querySelector('.modal__form--btn');
+// const contentContainer = document.querySelector('.modal__content');
+
+// const modalContent = document.createElement('div');
 
 document.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
@@ -27,9 +28,8 @@ closeModalBtn.forEach(el => {
   el.addEventListener('click', closeModal);
 });
 
-submitBtn.addEventListener('click', closeModal);
-
 function openModal(e) {
+  //   makeModalContent(e);
   modal.classList.remove('is-hidden');
   document.body.classList.add('no-scroll');
 }
@@ -39,5 +39,14 @@ function closeModal(e) {
 }
 
 // function makeModalContent(e) {
-// 	if (e.target)
+//   if (e.currentTarget.dataset.modalContent === 'product') {
+//     modalContent.innerHTML = productModal;
+//     contentContainer.append(modalContent);
+//   } else {
+//     modalContent.innerHTML = contactModal;
+//     contentContainer.append(modalContent);
+
+//     const submitBtn = document.querySelector('.modal__form--btn');
+//     submitBtn.addEventListener('click', closeModal);
+//   }
 // }
