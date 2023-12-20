4b4bc81a2607af6cea27957e9fe31c18246e3bb6
Yaroslav Klimas <yarklim@gmail.com>
2023-12-20 13:39:25 +0200

    update modal.js

--- js/modal.js
+++ js/modal.js
@@ -1,32 +1,43 @@
+import { contactModal, productModal } from './modal-content';
+
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('[data-modal]');
-const openModal = document.querySelectorAll('[data-modal-open]');
-const closeModal = document.querySelectorAll('[data-modal-close]');
+const openModalBtn = document.querySelectorAll('[data-modal-open]');
+const closeModalBtn = document.querySelectorAll('[data-modal-close]');
+const contentContainer = document.querySelector('.modal__content');
 const submitBtn = document.querySelector('.modal__form--btn');
 
 document.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
-    toggleModal();
+    closeModal();
   }
 });
 
 backdrop.addEventListener('click', e => {
   if (e.currentTarget === e.target) {
-    toggleModal();
+    closeModal();
   }
 });
 
-openModal.forEach(el => {
-  el.addEventListener('click', toggleModal);
+openModalBtn.forEach(el => {
+  el.addEventListener('click', openModal);
 });
 
-closeModal.forEach(el => {
-  el.addEventListener('click', toggleModal);
+closeModalBtn.forEach(el => {
+  el.addEventListener('click', closeModal);
 });
 
-submitBtn.addEventListener('click', toggleModal);
+submitBtn.addEventListener('click', closeModal);
 
-function toggleModal(e) {
-  modal.classList.toggle('is-hidden');
-  document.body.classList.toggle('no-scroll');
+function openModal(e) {
+  modal.classList.remove('is-hidden');
+  document.body.classList.add('no-scroll');
 }
+function closeModal(e) {
+  modal.classList.add('is-hidden');
+  document.body.classList.remove('no-scroll');
+}
+
+// function makeModalContent(e) {
+// 	if (e.target)
+// }
