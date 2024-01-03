7e527019ba216f26db34868988da93b8231ff9ff
Yaroslav Klimas <yarklim@gmail.com>
2024-01-03 12:01:19 +0200

    updated styles

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 import './mobile-menu';
 import './carousel';
-import './modal-content';
 import './modal';
 import './to-top';
--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,7 @@
-// import { contactModal, productModal } from './modal-content';
-
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('[data-modal]');
 const openModalBtn = document.querySelectorAll('[data-modal-open]');
 const closeModalBtn = document.querySelectorAll('[data-modal-close]');
-// const contentContainer = document.querySelector('.modal__content');
-
-// const modalContent = document.createElement('div');
 
 document.addEventListener('keydown', e => {
   if (e.code === 'Escape') {
@@ -29,7 +24,6 @@ closeModalBtn.forEach(el => {
 });
 
 function openModal(e) {
-  //   makeModalContent(e);
   modal.classList.remove('is-hidden');
   document.body.classList.add('no-scroll');
 }
@@ -37,16 +31,3 @@ function closeModal(e) {
   modal.classList.add('is-hidden');
   document.body.classList.remove('no-scroll');
 }
-
-// function makeModalContent(e) {
-//   if (e.currentTarget.dataset.modalContent === 'product') {
-//     modalContent.innerHTML = productModal;
-//     contentContainer.append(modalContent);
-//   } else {
-//     modalContent.innerHTML = contactModal;
-//     contentContainer.append(modalContent);
-
-//     const submitBtn = document.querySelector('.modal__form--btn');
-//     submitBtn.addEventListener('click', closeModal);
-//   }
-// }
